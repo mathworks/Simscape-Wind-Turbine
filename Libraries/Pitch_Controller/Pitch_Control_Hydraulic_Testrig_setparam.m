a46db6759fa0dcc6cbce29b60dce5c5b034c3881
Steve Miller <smiller@mathworks.com>
2019-11-02 11:17:52 +0100

    Initial commit R2016b

--- Libraries/Pitch_Controller/Pitch_Control_Hydraulic_Testrig_setparam.m
+++ Libraries/Pitch_Controller/Pitch_Control_Hydraulic_Testrig_setparam.m
@@ -3,8 +3,11 @@ function Pitch_Control_Hydraulic_Testrig_setparam(initialtuned)
 if strcmp(initialtuned,'Initial')
     evalin('base','Pitch_Kp = 7000;');
     evalin('base','Pitch_Ki = 10;');
+elseif strcmp(initialtuned, 'Linear');
+    evalin('base','Pitch_Kp = 81684;');
+    evalin('base','Pitch_Ki = 3544.6;');
 else
     evalin('base','Pitch_Kp = 100000;');
-    evalin('base','Pitch_Ki = 10;');
+    evalin('base','Pitch_Ki = 10;'); 
 end
     
